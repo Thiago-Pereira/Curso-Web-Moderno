ccd74271880a6cadec1418c95524f0422827d4fd
Thiago <thiago@localhost.localdomain>
2020-12-05 13:32:23 -0300

    first commit

---
+++ Js/fundamentos/comentario.js
@@ -0,0 +1,15 @@
+// comentário de uma linha
+console.log("Linha 1");
+
+/* 
+Comentário de 
+várias linhas
+*/
+console.log("Linha 2");
+
+/* 
+ * Comentário
+ * de 
+ * multiplas 
+ * linhas
+ */
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Curso-Web-Moderno
+# Curso de Web Moderno
+
+Aqui se encontra alguns arquivos do curso de web moderno realizado na udemy com algumas tecnologias web, dentre elas o NodeJs
